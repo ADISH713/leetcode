bb96070dff5eb39aea8631428b6801f01029cfec
ADISH  C P <adishadi7559@gmail.com>
2026-08-08 09:51:06 +0530

    Time: 4 ms (13.01%), Space: 58.3 MB (5.69%) - LeetHub

---
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var countNegatives = function(grid) {
+      // let count = 0;
+    // for(let i of grid){
+    //     for(let j of i){
+    //         if(j<0){
+    //             count++
+    //         }
+    //     }
+    // }
+    // return count
+    let count = grid.flat().filter(x=>x<0).length;
+    return count;
+};
