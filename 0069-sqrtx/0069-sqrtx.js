9b07411b281517eb986cbcc45280b667823ad29d
ADISH  C P <adishadi7559@gmail.com>
2026-09-03 10:26:12 +0530

    Time: 0 ms (100%), Space: 56.9 MB (23.21%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
+if (x < 2) return x;
+let r = x;
+while (r * r > x) {
+r = Math.floor((r + Math.floor(x / r)) / 2);
+}
+
+  return r;
+}
